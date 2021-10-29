aa4a0c0f1bec3a7aef0ff12dc2c18fcb78b45252
以牧 <13116157862@163.com>
2021-10-29 23:26:32 +0800

    JavaEE 线程池版 TCP 翻译程序

---
+++ JavaEE初阶(Web)/6.网络编程套接字/代码l练习/线程池版的TCP翻译程序/TcpDicServer.java
@@ -0,0 +1,34 @@
+package echoTcpProgram;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * @author wangyimu
+ * @Program SocketExer
+ * @create 2021-10-28-23:27
+ */
+public class TcpDicServer extends TcpThreadPoolEchoServer {
+    private HashMap<String,String> dict = new HashMap<>();
+
+    public TcpDicServer(int port) throws IOException {
+        super(port);
+        dict.put("cat","猫");
+        dict.put("dog","狗");
+        dict.put("windows","窗户");
+        dict.put("hello","你好");
+    }
+    // start 方法不变
+    // processConnection 方法也不变
+    // 只要重写 process 即可
+
+    @Override
+    public String process(String request) {
+        return dict.getOrDefault(request,"要查的词不存在!");
+    }
+
+    public static void main(String[] args) throws IOException {
+        TcpDicServer server = new TcpDicServer(9090);
+        server.start();
+    }
+}
