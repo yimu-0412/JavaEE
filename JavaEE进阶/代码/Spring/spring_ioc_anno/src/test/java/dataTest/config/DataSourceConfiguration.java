24a2746859eaae1ad3f827024e1806abd5b772cf
yimu-0412 <13116157862@163.com>
2022-01-03 23:36:21 +0800

    JavaEE Spring注解开发（新注解）

---
+++ JavaEE进阶/代码/Spring/spring_ioc_anno/src/test/java/dataTest/config/DataSourceConfiguration.java
@@ -0,0 +1,51 @@
+package dataTest.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.jolbox.bonecp.BoneCPDataSource;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import dataTest.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+
+/**
+ * @author wangyimu
+ * @Program Spring_5
+ * @create 2022-01-03-21:11
+ */
+
+// <context:property-placeholder location="jdbc.properties"/>
+@PropertySource("classpath:jdbc.properties")
+public class DataSourceConfiguration {
+    @Value("${jdbc.driver}")
+    private String driver;
+    @Value("${jdbc.url}")
+    private String url;
+    @Value("${jdbc.username}")
+    private String username;
+    @Value("${jdbc.password}")
+    private String password;
+
+    @Bean("dataSource") // 标注将该方法的返回值存储到Spring 容器中
+    public DataSource getDataSource() throws PropertyVetoException {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setDriverClass(driver);
+        dataSource.setJdbcUrl(url);
+        dataSource.setUser(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    @Bean("druidDataSource")
+    public DruidDataSource getDruid(){
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setDriverClassName(driver);
+        druidDataSource.setUrl(url);
+        druidDataSource.setUsername(username);
+        druidDataSource.setPassword(password);
+        return druidDataSource;
+    }
+}
