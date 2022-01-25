35c1b594c5f82fff72dc3a102462fb00f07165af
yimu-0412 <13116157862@163.com>
2022-01-25 23:38:04 +0800

    获取 get 方法中的参数

---
+++ JavaEE初阶(Web)/15.Servlet/代码/HttpServletRequest 核心方法/获取 Get 方法中的参数/GetParameterServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author wangyimu
+ * @Program 2022
+ * @create 2022-01-25-23:24
+ */
+@WebServlet("/getParameter")
+public class GetParameterServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+
+        String userId = req.getParameter("userId");
+        String classId = req.getParameter("classId");
+        resp.getWriter().write("userId: " + userId +
+                "; classId: " + classId );
+    }
+}
