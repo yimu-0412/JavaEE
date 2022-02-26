1f3448efba2fcf060d303ac7b6e4150d924a0d7b
yimu-0412 <13116157862@163.com>
2022-02-27 00:06:45 +0800

    DAO 层代码

---
+++ JavaEE进阶/代码/Spring/Spring/spring_mvc/src/main/java/yimu/dao/impl/UserDaoImpl.java
@@ -0,0 +1,15 @@
+package yimu.dao.impl;
+
+import yimu.dao.UserDao;
+
+/**
+ * @author wangyimu
+ * @Program Spring
+ * @create 2022-02-20-0:42
+ */
+public class UserDaoImpl implements UserDao {
+
+    public void save() {
+        System.out.println("save running···");
+    }
+}
