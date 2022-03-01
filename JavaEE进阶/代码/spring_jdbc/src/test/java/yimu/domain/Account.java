d5c3de929c5271a5193c408a85c5d2bfaabb8fae
yimu-0412 <13116157862@163.com>
2022-03-01 23:55:13 +0800

    jopo 代码

---
+++ JavaEE进阶/代码/spring_jdbc/src/test/java/yimu/domain/Account.java
@@ -0,0 +1,35 @@
+package yimu.domain;
+
+/**
+ * @author
+ * @Program
+ * @create 2022-02-27-21:55
+ */
+public class Account {
+    private String name;
+    private double money;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "name='" + name + '\'' +
+                ", money=" + money +
+                '}';
+    }
+}
