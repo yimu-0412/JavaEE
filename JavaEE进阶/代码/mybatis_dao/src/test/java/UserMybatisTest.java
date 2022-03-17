ddacbca260201b32a98835d37416a301ac0e7624
yimu-0412 <13116157862@163.com>
2022-03-17 23:50:10 +0800

    Mybatis的Dao层实现（代理开发方式）

---
+++ JavaEE进阶/代码/mybatis_dao/src/test/java/UserMybatisTest.java
@@ -0,0 +1,28 @@
+import domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * @author
+ * @Program
+ * @create 2022-03-17-20:16
+ */
+public class UserMybatisTest {
+
+    @Test
+    public void test1() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        List<User> list = sqlSession.selectList("userMapper.findAll");
+        System.out.println(list);
+        sqlSession.close();
+    }
+}
