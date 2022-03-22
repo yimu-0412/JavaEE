79984730fa64ff883e957fea2787769572f61d7d
yimu-0412 <13116157862@163.com>
2022-03-22 22:11:21 +0800

    实体类

---
+++ JavaEE进阶/MyBatis/代码/mybatis_multi/src/main/java/domain/User.java
@@ -0,0 +1,97 @@
+package domain;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author
+ * @Program
+ * @create 2022-03-22-11:17
+ */
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private Date birthday;
+
+    // 描当前用户存在那些订单
+    private List<Order> orderList;
+
+    // 描述的是当前用户具备那些角色
+    private List<Role> roleList;
+
+    public List<Role> getRoleList() {
+        return roleList;
+    }
+
+    public void setRoleList(List<Role> roleList) {
+        this.roleList = roleList;
+    }
+
+    public List<Order> getOrderList() {
+        return orderList;
+    }
+
+    public void setOrderList(List<Order> orderList) {
+        this.orderList = orderList;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    // @Override
+    // 一对多的 toString（）
+    /*public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", birthday=" + birthday +
+                ", orderList=" + orderList +
+                '}';
+    }*/
+
+
+    // 多对多的 toString（）
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", birthday=" + birthday +
+                ", roleList=" + roleList +
+                '}';
+    }
+}
