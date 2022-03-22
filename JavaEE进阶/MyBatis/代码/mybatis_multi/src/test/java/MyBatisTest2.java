748dfd3789addc1c22f26b8ae828f9f469e89ad7
yimu-0412 <13116157862@163.com>
2022-03-22 22:04:11 +0800

    数据表一对多测试代码

---
+++ JavaEE进阶/MyBatis/代码/mybatis_multi/src/test/java/MyBatisTest2.java
@@ -0,0 +1,35 @@
+import domain.User;
+import mapper.UserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * @author
+ * @Program
+ * @create 2022-03-22-21:55
+ */
+public class MyBatisTest2 {
+    @Test
+    // 一对多数据表查询
+    public void test2() throws IOException {
+
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<User> userList = mapper.findAll();
+        for (User user : userList) {
+            System.out.println(user);
+        }
+
+        sqlSession.close();
+    }
+}
