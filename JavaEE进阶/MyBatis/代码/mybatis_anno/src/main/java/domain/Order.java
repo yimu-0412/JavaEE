f0e40e94fc9b9973a100a5b25e497cef5f512d58
yimu-0412 <13116157862@163.com>
2022-03-23 18:22:44 +0800

    Mybatis的注解开发实体类

---
+++ JavaEE进阶/MyBatis/代码/mybatis_anno/src/main/java/domain/Order.java
@@ -0,0 +1,60 @@
+package domain;
+
+import java.util.Date;
+
+/**
+ * @author
+ * @Program
+ * @create 2022-03-22-11:20
+ */
+public class Order {
+    private int id;
+    private Date ordertime;
+    private double total;
+    private User user; // 当前订单属于那个用户
+
+    public Order() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getOrdertime() {
+        return ordertime;
+    }
+
+    public void setOrdertime(Date ordertime) {
+        this.ordertime = ordertime;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", ordertime=" + ordertime +
+                ", total=" + total +
+                ", user=" + user +
+                '}';
+    }
+}
---
+++ JavaEE进阶/MyBatis/代码/mybatis_anno/src/main/java/domain/User.java
@@ -0,0 +1,96 @@
+package domain;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author
+ * @Program
+ * @create 2022-03-15-22:13
+ */
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private Date birthday;
+
+    // 当前用户具有的订单
+    private List<Order> orderList;
+
+    // 当前用户具备的角色
+    private List<Role> roleList;
+
+
+    public List<Order> getOrderList() {
+        return orderList;
+    }
+
+    public void setOrderList(List<Order> orderList) {
+        this.orderList = orderList;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+
+
+    public List<Role> getRoleList() {
+
+        return roleList;
+    }
+
+    public void setRoleList(List<Role> roleList) {
+        this.roleList = roleList;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", roleList=" + roleList +
+                '}';
+    }
+
+    /*@Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", orderList=" + orderList +
+                '}';
+    }*/
+
+
+}
